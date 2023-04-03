4aba7ae653192fe00e949e70fe18cd061b503e74
itskaramaman <singhkaramjeetaman@gmail.com>
2023-04-03 12:52:34 -0700

    Product, Store, Order, OrderItem, ShippingAddress  model created

--- store/models.py
+++ store/models.py
@@ -1,3 +1,68 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Customer(models.Model):
+    """
+    Model for Customer info
+    """
+    customer = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Product(models.Model):
+    """
+    Model for Produt on store
+    """
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False)
+    image = models.ImageField()
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Order(models.Model):
+    """
+    Model for Order Info
+    """
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    date_orderd = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class OrderItem(models.Model):
+    """
+    Model for Order Item in Order
+    """
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.product.name)
+  
+
+class ShippingAddress(models.Model):
+    """
+    Model for shipping address of customer
+    """
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    state = models.CharField(max_length=200, null=False)
+    zipcode = models.CharField(max_length=200, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.address)
